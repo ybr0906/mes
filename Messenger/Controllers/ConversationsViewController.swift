25e0fc7d5b2d6d11a5ce9d7fd53d1ed977b04502
윤병록 <ybr@yunbyeonglog-ui-MacBookPro.local>
2021-02-28 17:12:23 +0900

    gone

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by 윤병록 on 2021/02/24.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn{
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+
+
+            
+            present(nav, animated: false)
+        }
+    }
+    
+
+}
+
---
+++ Messenger/Resources/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by 윤병록 on 2021/02/26.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat{
+        return self.frame.origin.y
+    }
+    public var bottom: CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat{
+        return self.frame.origin.x
+    }
+    public var right: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
